98573eba7dd4bbfd46cb1c59aa345030950bc046
Richard Lovell <cassh3rn@talktalk.net>
2022-09-26 16:46:44 +0100

    add rodzina.js

---
+++ rodzina.js
@@ -0,0 +1,12 @@
+const rodzina = {
+    "krewni": "relatives",
+    "wnuki": "grandchildren",
+    "bliźnięta": "twins",
+    "ojczym": "stepfather",
+    "teść": "father-in-law",
+    "teściowa": "mother-in-law",
+    "szwagier": "brother-in-law",
+    "szwagierka": "sister-in-law",
+    "siostrzenica": "niece",
+    "siostrzeniec": "nephew"
+}
